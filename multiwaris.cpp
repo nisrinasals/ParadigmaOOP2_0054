6e1b2e383ad24d6adcc1c7ccd11b75d336e48190
nisrinasals <nisrinahikmawan@gmail.com>
2024-05-27 15:14:42 +0700

    membuat kelas pekerja dan pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -12,3 +12,23 @@ public:
     }
 };
 
+class pekerja : virtual public orang {
+public:
+
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
